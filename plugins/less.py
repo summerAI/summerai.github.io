1e232bb5f5d16dc3977293c127db13f99486a253
Manuel Ebert <manuel@1450.me>
2015-07-27 13:12:44 +0200

    Refactoring and bookshelf

--- plugins/less.py
+++ plugins/less.py
@@ -1,9 +1,11 @@
 import subprocess
-from glob import glob
+
+OUTPUT = ("error", "landing", "bookshelf")
 
 
 def postBuild(site):
-    for filename in glob('%s/static/less/*less' % site.paths['build']):
+    for output in OUTPUT:
+        filename = '{}/static/less/{}.less'.format(site.paths['build'], output)
         root_name = filename.rsplit('.', 1)[0]
         src = filename
         dest = "%s.css" % root_name.replace("/less/", "/css/")
